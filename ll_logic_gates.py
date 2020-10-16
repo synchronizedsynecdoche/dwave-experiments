26d54563eac5cf234c7f091966faecd766565cfd
Patrick Kelly <kelly237@purdue.edu>
2020-10-16 21:08:34 +0000

    implemented Ising models from UCLA paper

--- ll_logic_gates.py
+++ ll_logic_gates.py
@@ -26,5 +26,9 @@ def generateRealSamples(qubo, num_reads=10):
 
 lNAND_qubo = {('x_1', 'x_2') : 1, ('z','z'): -1}
 lNOR_qubo = {('x_1', 'x_1'): 2, ('x_2', 'x_2'):2, ('z', 'z'):-1}
-sampleset = generateSimulatedSamples(lNOR_qubo, num_reads=100)
+# trying Ising models from http://eda.ee.ucla.edu/pub/C166.pdf
+l2_qubo = {('x','x'): - 1, ('y','y'):-1, ('z','z'): 2,
+           ('x','z'): -2, ('y', 'z'):-2, ('x','y'): 1}
+
+sampleset = generateSimulatedSamples(l2_qubo, num_reads=100)
 print(sampleset)
